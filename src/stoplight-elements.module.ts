1f5b788874cd826c9ae33645b3f64697f5acc73b
Nawbc <wanghan9423@outlook.com>
2023-03-18 16:57:37 +0800

    s

--- src/stoplight-elements.module.ts
+++ src/stoplight-elements.module.ts
@@ -1,3 +1,24 @@
+import { INestApplication } from '@nestjs/common';
+import type { OpenAPIObject } from '@nestjs/swagger';
+
+export interface StoplightElementsOptions {}
+
+/**
+ *
+ * @example
+ * ```
+ * StoplightElementsModule.setup('/docs',app, xxxDoc, {
+ *
+ *
+ * })
+ *
+ * ```
+ */
 export class StoplightElementsModule {
-  
+  public static async setup(
+    path: string,
+    app: INestApplication,
+    document: OpenAPIObject,
+    options?: StoplightElementsOptions,
+  ) {}
 }
